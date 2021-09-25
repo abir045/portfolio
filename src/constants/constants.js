8624615e33ba0a94edf7c5c4d11da03f3dc7456c
Salekin Imran <abir045@gmail.com>
2021-09-25 15:47:49 +0600

    updated images for projects

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -3,7 +3,7 @@ export const projects = [
     title: "Covid-19 Dashboard",
     description:
       "This is a covid-19 tracker App submitted in a Hackathon built with bootstrap and Js",
-    image: "/images/covid-19.jpg",
+    image: "/images/covid19.JPG",
     tags: ["Bootstrap", "JS", "HTML", "CSS3"],
     source: "https://abir045.github.io/covid19/",
     visit: "https://github.com/abir045/covid19",
@@ -12,7 +12,7 @@ export const projects = [
   {
     title: "Limousine Booking App",
     description: "Limousine booking App",
-    image: "/images/limo.jpg",
+    image: "/images/pearson.JPG",
     tags: ["Node", "Express", "Google Maps API"],
     source: "https://github.com/abir045/LomousineTours",
     visit: "https://pearsonairportlimousines.com/",
@@ -21,7 +21,7 @@ export const projects = [
   {
     title: "E Commerce",
     description: "E-Commerce WEbsite",
-    image: "/images/alvaro.jpg",
+    image: "/images/alvaro-shop.JPG",
     tags: ["React", "SCSS", "JS", "Stripe API"],
     source: "https://github.com/abir045/crwn-clothing",
     visit: "https://demostore2.herokuapp.com/",
